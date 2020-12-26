c2ac382be78f70641af0c0b96c437ad6018d679a
laon <475281441@qq.com>
2020-12-26 11:10:51 +0800

    更新支持修改网关

--- src/Client.php
+++ src/Client.php
@@ -10,30 +10,51 @@ class Client
 {
 	protected $version = '1.0';
 	
-	public    $client;
+	protected $client;
 	
 	protected $appid;
 	
 	protected $key;
 	
-	//	public    $gateway = 'https://api.mimaapp.com/';
-	public $gateway = 'http://localhost:8080/';
+	public    $gateway = 'https://api.mimaapp.com/';
 	
-	public $sign;
+	public    $sign;
 	
 	public function __construct($appid, $key)
 	{
-		$this->appid  = $appid;
-		$this->key    = $key;
-		$this->client = new GuzzleClient(
+		$this->appid = $appid;
+		$this->key   = $key;
+		$this->sign  = new Sign($key);
+	}
+	
+	public function getClient(): GuzzleClient
+	{
+		return $this->client = $this->client ?: new GuzzleClient(
 			[
 				'base_uri' => $this->gateway,
 				'headers'  => [
-					'appid'        => $appid,
+					'appid'        => $this->appid,
 					'Content-Type' => 'application/json'
 				]
 			]);
-		$this->sign   = new Sign($key);
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getGateway(): string
+	{
+		return $this->gateway;
+	}
+	
+	/**
+	 * @param string $gateway
+	 * @return Client
+	 */
+	public function setGateway(string $gateway): Client
+	{
+		$this->gateway = $gateway;
+		return $this;
 	}
 	
 	/**
@@ -46,7 +67,7 @@ class Client
 	public function post($url, array $data): array
 	{
 		$data    = $this->sign->sign($data);
-		$result  = $this->client->request('post', ltrim($url, '/'), ['body' => json_encode($data)]);
+		$result  = $this->getClient()->request('post', ltrim($url, '/'), ['body' => json_encode($data)]);
 		$content = $result->getBody()->getContents();
 		return json_decode($content, true);
 	}
@@ -61,7 +82,7 @@ class Client
 	public function get($url, array $data): array
 	{
 		$data    = $this->sign->sign($data);
-		$result  = $this->client->request('get', ltrim($url, '/'), ['query' => http_build_query($data)]);
+		$result  = $this->getClient()->request('get', ltrim($url, '/'), ['query' => http_build_query($data)]);
 		$content = $result->getBody()->getContents();
 		return json_decode($content, true);
 	}
--- src/example/test.php
+++ src/example/test.php
@@ -6,11 +6,54 @@ require '../../vendor/autoload.php';
 
 $client = new Client('791212754', 'LWFug7TGXWc0xhQSapL5afIoJsB5Y559');
 
-$result = $client->get('open/order/list', ['aaa' => 'bbb', 'ccc' => 'ddd']);
+$client->setGateway('http://localhost:8080/');
+//
+//$result = $client->get('open/order/list', ['aaa' => 'bbb', 'ccc' => 'ddd']);
+//
+//print_r($result);
+//
+//
+//$result = $client->post('open/order/log', ['dddddddd' => 'bbb', 'ccc' => 'ddd']);
+//
+//print_r($result);
 
-print_r($result);
+//[
+//			                'order_num|订单号'       => 'checkOrderNum',
+//			                'game_type|游戏类型'      => 'require|eq:1',//1:王者荣耀
+//			                'ringer_type|代练类型'    => 'require|eq:1',//1:段位代练
+//			                'price|订单价格'          => 'require|gt:0',//元
+//			                'title|代练标题'          => 'require|length:1,255',
+//			                'partition_id|游戏区服'   => 'require|gt:0',//游戏区服
+//			                'user_remark|订单备注'    => 'length:1,800',
+//			                'speed_deposit|效率保证金' => 'gt:0',
+//			                'safe_deposit|安全保证金'  => 'gt:0',
+//			                'estimated_time|代练时长' => 'require|gt:0',//分钟
+//			                'game_account|游戏帐号'   => 'require|length:1,50',
+//			                'game_password|游戏密码'  => 'require|length:1,50',
+//			                'buyer_phone|买家手机号'   => ['require', 'regex' => BaseValidate::validatePhoneReg],
+//			                'appoint_id|指定密马代练员'  => BaseValidate::validateBigInt,
+//		                ]
+
+//$result = $client->post('open/order/build',
+//                        [
+//	                        'game_type'      => '1',
+//	                        'ringer_type'    => '1',
+//	                        'price'          => '20',
+//	                        'title'          => '防沉迷 白银3 2 星 - 黄金1 3星  铭文120 关定位',
+//	                        'partition_id'   => '3233',
+//	                        'user_remark'    => '',
+//	                        'speed_deposit'  => '25',
+//	                        'safe_deposit'   => '25',
+//	                        'estimated_time' => '600',
+//	                        'game_account'   => 'qq123',
+//	                        'game_password'  => 'pwd123',
+//	                        'buyer_phone'    => '13560800000',
+//	                        'appoint_id'     => '0',
+//                        ]);
+//
+//print_r($result);
 
 
-$result = $client->post('open/order/log', ['dddddddd' => 'bbb', 'ccc' => 'ddd']);
+$result = $client->get('/open/report_reason', ['game_type'      => '1']);
 
 print_r($result);
